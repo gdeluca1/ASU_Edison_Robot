6e2feb870bd21f7ef30020f7194a31c21a4b29a7
Gennaro De Luca <gdeluca1@asu.edu>
2016-06-23 18:28:13 -0700

    Removed broken robot startup command.

--- config_files/on_boot.sh
+++ config_files/on_boot.sh
@@ -4,4 +4,3 @@
 /usr/bin/hciconfig hci0 piscan
 systemctl start obex
 nohup python /home/root/ASU_Edison_Robot/config_files/ip.py &
-nohup ./home/root/ASU_Edison_Robot/run.sh &
--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ while true; do
 # Kill any instances of the sketch that were previously running.
 # The sketch currently needs to be restarted each time to allow
 # the socket to be connected to.
-pidof sketch.elf | xargs kill
+pidof sketch_old.elf | xargs kill
 
 # Start the arduino sketch.
-./ArduinoFiles/sketch.elf /dev/ttyS0 /dev/ttyS0 &
+./ArduinoFiles/sketch_old.elf /dev/ttyS0 /dev/ttyS0 &
 
 # Start the node file.
 node main.js
